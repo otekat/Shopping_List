64d4126ef882db8b48318182b73c0088767f0246
otekat <heskmanuel@gmail.com>
2017-08-16 17:08:12 +0300

    added shopping_app

---
+++ models/Shopping_App.py
@@ -0,0 +1,24 @@
+class ShoppingList():
+    def __init__(self):
+        self.users = {}
+        self.users_shoppinglist = []
+
+    def user_register(self, user):
+        if user.email in self.users.keys():
+            return False
+        else:
+            self.users.update({user.email: user})
+            self.users_shoppinglist.append(self.users)
+            return True
+
+    def get_user(self, email):
+        if email in self.users.keys():
+            return self.users[email]
+        return None
+
+    def user_login(self, email, password):
+        if email in self.users.keys():
+            if self.users[email].password == password:
+                return True
+
+
