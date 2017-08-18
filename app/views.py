86a5811f25f46ca1d3fdc455bab253df5119e7b5
otekat <heskmanuel@gmail.com>
2017-08-18 16:54:42 +0300

    adding views

--- app/views.py
+++ app/views.py
@@ -1,36 +1,45 @@
 from app import app
-from flask import render_template, redirect, url_for, request, session
+from flask import render_template, redirect, url_for, request, session, flash
+
+from app.models.User import User
+from app.models.application import Application
+
+application = Application()
 
 
 @app.route('/')
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'Emma' or request.form['password'] != 'emma1234':
-            error = 'Invalid credentials. Please try again.'
-        else:
-            session['logged_in'] = True
-            return redirect(url_for('home'))
-    return render_template('login.html', error=error)
-
-
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     error = None
     if request.method == 'POST':
-        if request.form['Firstname'] and request.form['Lastname'] and request.form['Email'] and request.form[
-            'password'] and request.form['confirm password']:
-            user = User(request.form['Firstname'] and request.form['Lastname'] and request.form['Email'] and
-                        request.form[
-                            'password'] and request.form['confirm password'])
-            return redirect[url_for('login')]
-    return render_template('signup.html')
+        email_ = request.form['email']
+        password_ = request.form['password']
+        confirm_password_ = request.form['confirm_password']
+        if email_ and password_ and confirm_password_:
+            if password_ == confirm_password_:
+                user = User(email_, password_)
+                if application.create_user(user):
+                    return redirect(url_for('login'))
+            flash('your passwords do not match')
+        error = 'Fill in the missing attributes'
+    return render_template('signup.html', error=error)
 
 
-@app.route('/logout')
-def logout():
-    session.pop(logged_in, None)
-    return rediect(url_for('home'))
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    error1 = None
+    if request.method == 'POST':
+        if request.form['email'] and request.form['password']:
+            if application.login(request.form['email'], request.form['password']):
+                # session['email'] = request.form['email']
+                return redirect(url_for('home'))
+            error = 'Invalid credentials. please try again.'
+            return render_template('login.html', error=error)
+        return render_template('login.html', error1=error1)
+    return render_template('login.html')
 
-    # @app.route('/create_list',methods=['GET', 'POST'])
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
