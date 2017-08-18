38809a33aefc8982064240b747abfdaf9dba8318
otekat <heskmanuel@gmail.com>
2017-08-18 11:58:36 +0300

    adding models

--- app/views.py
+++ app/views.py
@@ -15,13 +15,24 @@ def login():
     return render_template('login.html', error=error)
 
 
+@app.route('/signup',methods=['GET', 'POST'])
 def signup():
     error = None
     if request.method == 'POST':
-        if request.form['Firstname'] and request.form['Lastname'] and request.form['Email'] and request.form[
-            'password'] and request.form['confirm password']:
-            user = User(
-                request.form['Firstname'] and request.form['Lastname'] and request.form['Email'] and request.form[
+        if request.form['Firstname'] and request.form['Lastname'] and request.form['Email'] and request.form['password'] and request.form['confirm password']:
+            user = User(request.form['Firstname'] and request.form['Lastname'] and request.form['Email'] and request.form[
                     'password'] and request.form['confirm password'])
             return redirect[url_for('login')]
     return render_template('signup.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop(logged_in, None)
+    return rediect(url_for('home'))
+
+@app.route('/create_list',methods=['GET', 'POST'])
+def create_list():
+    error = None
+    if request.method == 'POST'
+
