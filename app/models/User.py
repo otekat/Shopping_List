6b5cef9a4bf7d82a27e724f2051ff5feb76717ff
otekat <heskmanuel@gmail.com>
2017-08-18 16:52:40 +0300

    adding user

--- app/models/User.py
+++ app/models/User.py
@@ -1,26 +1,6 @@
-import random
-
 class User:
-    def __init__(self, username, email, password):
-        self.username = username
+    def __init__(self, email, password):
         self.email = email
         self.password = password
         self.ShoppingList = {}
 
-    def create_shoppinglist(self, title, description):
-        if title is not self.ShoppingList.keys():
-            self.ShoppingList[title] = description
-            self.ShoppingList.keys()
-            return True
-        else:
-            return False
-
-    def delete_shoppinglist(self, title):
-        if title in self.ShoppingList.keys():
-            del self.ShoppingList[title]
-        return self.ShoppingList
-
-    def update_shoppinglist(self, title, description):
-        if title in self.ShoppingList.keys():
-            self.ShoppingList[title] = description
-            return self.ShoppingList
