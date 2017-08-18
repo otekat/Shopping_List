03763dc2046789d7b3d501af16d40202402018e6
otekat <heskmanuel@gmail.com>
2017-08-18 16:51:46 +0300

    adding application

--- app/models/application.py
+++ app/models/application.py
@@ -1,25 +1,31 @@
-import random
-
 class Application:
+    def __init__(self):
+        """
+        constructor method
+        """
+        self.users = {}
 
-    def __init__(self, users):
-        self.users = users
-
-    def create_user (self, user):
+    def create_user(self, user):
         if user.email not in self.users.keys():
-            self.users[user.email]=user
+            self.users[user.email] = user
             return True
         return False
 
-    def login (self, email, password):
-        if email in self.users[email].password == password:
-            return True
-         return False
-
-    def user_account(self,email):
-        if email in self.users:
+    def get_user(self, email):
+        if email in self.users.keys():
             return self.users[email]
         return False
 
+    def signup(self, user):
+        if user.email in self.users.keys():
+            return False
+        else:
+            self.users[user.email] = user
+            return True
 
-
+    def login(self, email, password):
+        if email in self.users.keys():
+            user = self.users[email]
+            if user.password == password:
+                return True
+            return False
